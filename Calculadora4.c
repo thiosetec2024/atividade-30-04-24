b1d19befdac7c0bc64e0cb571eaf54dbf23d21d0
THIAGO DE OLIVEIRA SILVA <163483926+thiosetec2024@users.noreply.github.com>
2024-05-04 19:27:59 -0300

    agora vai

--- Calculadora4.c
+++ Calculadora4.c
@@ -3,47 +3,112 @@
 #include<locale.h>
 
 int main() {
-    setlocale(LC_ALL, "Portuguese");
-
+    setlocale(LC_ALL , "Portuguese");
     char operador;
     float num1, num2, resultado;
+    int validar1, validar2; 
+    char continuar;
 
     do {
-        printf("Digite um operador (+, -, *, /): \n");
-        scanf("%c", &operador); 
+                printf(" _______________________________________________\n");
+            	printf("|                  CALCULADORA                  |\n");
+                printf("|                    THIAGO                     |\n");
+                printf("|_______________________________________________|\n");
+        printf("\n" );
+
         
-       if (operador != ("+", "-" , "*" ,"/")) {
-           printf("Operador inválido! Digite novamente \n");
-       }
-    } while (operador = ( + , - , * , /));
+        do {
+            printf("Digite um numero: ");
+            validar1 = scanf("%f", &num1);
 
-    printf("Digite um número: \n");
-    scanf("%f", &num1);
-
-    printf("Digite outro número: \n");
-    scanf("%f", &num2);
-
-    switch (operador) {
-        case '+':
-            resultado = num1 + num2;
-            break;
-        case '-':
-            resultado = num1 - num2;
-            break;
-        case '*':
-            resultado = num1 * num2;
-            break;
-        case '/':
-            if (num2 == 0) {
-                printf("Divisão por zero não é permitida.\n");
-                return 1;
-            } else {
-                resultado = num1 / num2;
+           
+            if (validar1 != 1) {
+            	printf("  _______________________________________________\n");
+            	printf(" |>>>>>>>>>>>>>>>>>>>ATENÇÃO<<<<<<<<<<<<<<<<<<<<<|\n");
+                printf(" |       Favor informar apenas numeros!!!!       |\n");
+                printf(" |_______________________________________________|\n");
+                fflush(stdin); 
             }
-            break;
-    }
+        } while (validar1 != 1);
 
-    printf("Resultado: %.2f\n", resultado);
+                printf("  _______________________________________________\n ");
+                printf("|                                               |\n ");
+                printf("| Informe a operação que gostaria de efetuar    |\n ");
+                printf("|             + soma , - subtração ,            |\n ");
+                printf("|        * multiplicação  , / divisão           |\n ");
+                printf("|_______________________________________________|\n ");
+                printf("\n" );
+                printf("Operação: ");
+       
+        scanf(" %c", &operador);
+    
+         
+        if (operador!='-'  && operador!='+'  && operador!='*'  && operador!='/') {
+            	printf(" _______________________________________________\n");
+            	printf("|>>>>>>>>>>>>>>>>>>>ATENÇÃO<<<<<<<<<<<<<<<<<<<<<|\n");
+                printf("|              Operação invalida                |\n");
+                printf("|_______________________________________________|\n");
+                printf("\n" );
+        } else {
+            
+            do {
+            	printf("\n");
+                printf("Digite outro numero: ");
+               
+                validar2 = scanf("%f", &num2);
+            
+                
+                if (validar2 != 1) {
+                printf(" _______________________________________________\n");
+            	printf("|>>>>>>>>>>>>>>>>>>>ATENÇÃO<<<<<<<<<<<<<<<<<<<<<|\n");
+                printf("|       Favor informar apenas numeros!!!!       |\n");
+                printf("|_______________________________________________|\n");
+                    fflush(stdin); 
+                }
+            } while (validar2 != 1);
 
-    return 0;
+            if (operador == '/' && num2 == 0) {
+                printf(" _______________________________________________\n");
+            	printf("|>>>>>>>>>>>>>>>>>>>ATENÇÃO<<<<<<<<<<<<<<<<<<<<<|\n");
+                printf("|      NÃO É POSSIVEL DIVISÃO POR ZERO!!!!      |\n");
+                printf("|_______________________________________________|\n");
+            } else {
+                switch(operador) {
+                    case '+':
+                        resultado = num1 + num2;
+                        break;
+                    case '-':
+                        resultado = num1 - num2;
+                        break;
+                    case '*':
+                        resultado = num1 * num2;
+                        break;
+                    case '/':
+                        resultado = num1 / num2;
+                        break;
+                }
+                printf( "\n" );
+                
+                printf("O resultado da operação é: %.2f\n", resultado);
+            }
+        }
+        printf( "\n" ); 
+        printf(" _______________________________________________\n");
+        printf("|                                               |\n");
+        printf("|     GOSTARIA DE CONTINUAR CALCULANDO!!!!      |\n");
+        printf("|_______________________________________________|\n");
+        printf( "\n" );
+        printf("informe S / N : ");
+        scanf(" %c", &continuar);
+
+    } while (continuar == 'S' || continuar == 's');
+        printf( "\n" );
+        printf(" _______________________________________________\n");
+        printf("|            Obrigado por utilizar              |\n");
+        printf("|            Calculadora do THIAGO!             |\n");
+        printf("|_______________________________________________|\n");
+        printf( "\n" );
+
+    
 }
+
